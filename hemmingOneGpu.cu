22e91fd59041ea95e25e805a4bc9a6d4015517a7
JKarchut <jkarchut00@gmail.com>
2021-11-23 19:42:20 +0100

    adssad

--- hemmingOneGpu.cu
+++ hemmingOneGpu.cu
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
- 
-void parseNumber(  int *arr, std::string number, int bitsPerInt)
+void parseNumber(unsigned int *arr, std::string number, int bitsPerInt)
 {
     int bitPos = 0;
     int arrPos = 0;
-    int pomValue = 0;
+    unsigned int pomValue = 0;
     for(int x = 0; x < number.length();x++)
     {
-        if(bitPos < bitsPerInt)
+        if(bitsPerInt > bitPos)
         {
-            pomValue = (pomValue * 2) + ((int)(number[x] - '0'));
+            pomValue += (unsigned int)(number[x] - '0') << bitPos;
             bitPos++;
         }
         else
@@ -28,24 +27,20 @@ void parseNumber(  int *arr, std::string number, int bitsPerInt)
     }
 }
 
-__global__ void findPairs(  int *arr, int n, int l)
+__global__ void findPairs(unsigned int *arr, int n, int l)
 {
-    int id = blockIdx.x * 1024 + threadIdx.x;
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
     int diff, pom;
-    for(int x = id + 1; x < n; x++)
+    for(int x = index + 1; x < n; x++)
     {
         diff = 0;
         for(int y = 0; y < l; y++)
         {
-            pom = 0;
-            pom = (arr[(id * l) + y]^arr[(x * l) + y]);    
-            while(pom != 0)
-            {
-                diff += (pom & 1);
-                pom = (pom >> 1);
-                if(diff > 1)
-                    break;
-            }
+            pom = (arr[index * l + y]^arr[x * l + y]);
+            if(pom > 0 && (pom & (pom - 1)) == 0)
+                diff++;
+            else if(pom > 0)
+                diff = 2;
             if(diff > 1)
                 break;
         }
@@ -54,39 +49,34 @@ __global__ void findPairs(  int *arr, int n, int l)
             printf("%d %d\n",id,x);
         }
     }
-    
 }
 
-int main(int argc, char** argv)
+int32_t main(int argc, char** argv)
 {
     std::ifstream data(argv[1]);
     int n;
     int l;
     data >> n;
     data >> l;
-    int bitsPerInt =  15;
+    int bitsPerInt =  sizeof(unsigned int) * 8 - 1;
     int taken = l / bitsPerInt;
     if(l % bitsPerInt != 0)
         taken++;
-    int* arr = new   int[n * taken];
-    memset(arr,0,taken * n * sizeof(int));
+    unsigned int* arr;
+    cudaMallocManaged(&arr, n*sizeof(unsigned int) * taken);
+    memset(arr,0,taken * n * sizeof(unsigned int));
     std::string number;
     int arrPos = 0;
     while(data >> number)
     {
-        parseNumber(&arr[taken * arrPos], number, bitsPerInt);
-        printf("%d\n",arr[taken * arrPos]);
+        parseNumber(&arr[arrPos * taken], number, bitsPerInt);
         arrPos++;
     }
-    printf("%d %d\n",n,taken);
-    int* arr_d;
-    cudaMalloc(&arr_d, n * taken * sizeof(int));
-    cudaMemcpy(arr_d,arr, n * taken * sizeof(int), cudaMemcpyHostToDevice);
     int threadCount = 1024;
-    int blockSize = (n / threadCount) + 1;
-    findPairs<<<blockSize,threadCount>>>(arr_d,n,taken);
+    int blockCount = (n / 1024) + 1;
+    findPairs<<<blockCount,threadCount>>>(arr,n,taken);
     cudaDeviceSynchronize();
     data.close();
-    cudaFree(arr_d);
-    delete[] arr;
+    cudaFree(arr);
+    return 0;
 }
