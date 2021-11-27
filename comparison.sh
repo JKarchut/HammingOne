0c98f3b71b605d1205000387c11fb3e37db412ff
JKarchut <jkarchut00@gmail.com>
2021-11-27 17:21:46 +0100

    all

--- comparison.sh
+++ comparison.sh
@@ -3,11 +3,13 @@ echo "--- TEST ---" >> time.txt
 make
 echo "dataset 100k vectors of 1000 long" >> time.txt
 
+echo "--- CPU ---" >> time.txt
 echo "CPU"
 CPU_START_TIME=$(date +%s%N | cut -b1-13)
 ./hammingOneCpu test.txt > output_cpu.txt time.txt
 CPU_END_TIME=$(date +%s%N | cut -b1-13)
 
+echo "--- GPU ---" >> time.txt
 echo "GPU"
 GPU_START_TIME=$(date +%s%N | cut -b1-13)
 ./hammingOneGpu test.txt > output_gpu.txt time.txt
