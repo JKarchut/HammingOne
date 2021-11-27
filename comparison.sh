5bfbeb2e6ffc5893ecb777c01b068d01c7c43e6b
JKarchut <jkarchut00@gmail.com>
2021-11-27 17:27:21 +0100

    change formating

--- comparison.sh
+++ comparison.sh
@@ -1,3 +1,4 @@
+echo "" >> time.txt
 echo "--- TEST ---" >> time.txt
 
 make
@@ -8,16 +9,16 @@ echo "CPU"
 CPU_START_TIME=$(date +%s%N | cut -b1-13)
 ./hammingOneCpu test.txt > output_cpu.txt time.txt
 CPU_END_TIME=$(date +%s%N | cut -b1-13)
+echo "CPU total $(($CPU_END_TIME - $CPU_START_TIME)) ms" >> time.txt
 
 echo "--- GPU ---" >> time.txt
 echo "GPU"
 GPU_START_TIME=$(date +%s%N | cut -b1-13)
 ./hammingOneGpu test.txt > output_gpu.txt time.txt
 GPU_END_TIME=$(date +%s%N | cut -b1-13)
+echo "GPU total $(($GPU_END_TIME - $GPU_START_TIME)) ms" >> time.txt
 
 echo `./verify output_cpu.txt output_gpu.txt` >> time.txt
 
 make clean
 
-echo "CPU total $(($CPU_END_TIME - $CPU_START_TIME)) ms" >> time.txt
-echo "GPU total $(($GPU_END_TIME - $GPU_START_TIME)) ms" >> time.txt
